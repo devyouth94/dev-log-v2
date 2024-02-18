6006caf451ba1bf3b85144664b0e24a6a739acc4
devyouth94 <devyouth94@gmail.com>
2024-02-18 17:12:37 +0900

    Feat: 구글 서치 콘솔 등록

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="BmJKKZAJXdOObZSfbVTD7ze2oCPBWDHumGy-YOE3Erk"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

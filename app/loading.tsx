8797dbaed80d2e4936e747654e5007adc9a37b43
devyouth94 <devyouth94@gmail.com>
2025-01-22 19:32:32 +0900

    refactor: 로딩 페이지 백그라운드 컬러 설정

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import Icon from "src/components/shared/icon";
 
 const LoadingPage = () => {
   return (
-    <div className="flex h-dvh w-full items-center justify-center">
+    <div className="flex h-dvh w-full items-center justify-center bg-white">
       <Icon name="Loader" className="animate-spin" size={20} />
     </div>
   );

275e391820e382811417297adf0c72e912a314f2
devyouth94 <devyouth94@gmail.com>
2025-01-22 18:49:54 +0900

    refactor: 유틸함수 정리 및 수정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,20 +1,7 @@
 import { format } from "date-fns";
 
-import { IPlaylistItem } from "~/types/youtube";
-
-export const getRecentUpdatedDate = (data: IPlaylistItem[]) => {
-  if (!data.length) return "-";
-
-  const dateMap = data.map(({ publishedAt }) =>
-    new Date(publishedAt).getTime(),
-  );
-  const latestDate = new Date(Math.max(...dateMap));
-
-  return format(latestDate, "yyyy. MM. dd");
-};
-
 export const getRenderedDate = (input: string | number) => {
   const date = new Date(input);
 
-  return format(date, "yyyy. MM. dd");
+  return format(date, "yy.MM.dd");
 };
